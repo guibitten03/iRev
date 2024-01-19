a85d56ad3349460ceff878e2db5391286cd08a66
guibitten03 <bittenkourt.gmf@gmail.com>
2024-01-19 22:50:18 +0000

    Results

---
+++ models/pmf_bash.sh
@@ -0,0 +1,4 @@
+python3 train_pmf.py train --dataset=Musical_Instruments_data --emb_opt=word2vec
+python3 train_pmf.py train --dataset=Office_Products_data --emb_opt=word2vec
+python3 train_pmf.py train --dataset=Tamp_data --emb_opt=word2vec
+python3 train_pmf.py train --dataset=Tucso_data --emb_opt=word2vec
