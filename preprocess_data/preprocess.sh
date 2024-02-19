4aa1957aa7b8146a34764fe1c05f229a842394f3
guibitten03 <bittenkourt.gmf@gmail.com>
2024-02-19 16:53:38 +0000

    Finetunning Processing and Trainning RecSys

--- preprocess_data/preprocess.sh
+++ preprocess_data/preprocess.sh
@@ -1,3 +1,5 @@
+python3 pro_data.py .data/Digital_Music_5.json amazon language_models/GoogleNews-vectors-negative300.bin word2vec > info/DIGITAL_MUSIC_w2v
+
 python3 pro_data.py .data/Tampa.csv yelp language_models/GoogleNews-vectors-negative300.bin word2vec > info/TAMPA_INFO_w2v
 
 python3 pro_data.py .data/Tucson.csv yelp language_models/GoogleNews-vectors-negative300.bin word2vec > info/Tucson_INFO_w2v
